e8984f68041f040063e7856d8c1b33be9180b76e
Dirk Eddelbuettel <edd@debian.org>
2020-01-11 13:28:01 -0600

    (possibly fatal) check for Rcpp and RcppArmadillo now opt-in

--- inst/include/RcppEnsmallen.h
+++ inst/include/RcppEnsmallen.h
@@ -18,8 +18,10 @@
 #ifndef RcppEnsmallen__RcppEnsmallen__h
 #define RcppEnsmallen__RcppEnsmallen__h
 
-#if defined(Rcpp_hpp) && defined(RcppArmadillo__RcppArmadillo__h)
-#error "Do not include either 'Rcpp.h' or 'RcppArmadillo.h'. Please only use 'RcppEnsmallen.h'."
+#ifdef RCPPENSMALLEN_HEADER_CHECK
+  #if defined(Rcpp_hpp) && defined(RcppArmadillo__RcppArmadillo__h)
+    #error "Do not include either 'Rcpp.h' or 'RcppArmadillo.h'. Please only use 'RcppEnsmallen.h'."
+  #endif
 #endif
 
 // Enable RcppArmadillo and Rcpp
