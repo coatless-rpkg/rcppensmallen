f79aa89c0255bf700710936e8fad920d04aeb442
James Balamuta <balamut2@illinois.edu>
2020-01-14 10:52:40 -0600

    Remove the header check

--- inst/include/RcppEnsmallen.h
+++ inst/include/RcppEnsmallen.h
@@ -18,12 +18,6 @@
 #ifndef RcppEnsmallen__RcppEnsmallen__h
 #define RcppEnsmallen__RcppEnsmallen__h
 
-#ifdef RCPPENSMALLEN_HEADER_CHECK
-  #if defined(Rcpp_hpp) && defined(RcppArmadillo__RcppArmadillo__h)
-    #error "Do not include either 'Rcpp.h' or 'RcppArmadillo.h'. Please only use 'RcppEnsmallen.h'."
-  #endif
-#endif
-
 // Enable RcppArmadillo and Rcpp
 #include <RcppArmadillo.h>
 
